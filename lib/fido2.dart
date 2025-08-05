0a852b8dcf2cff21c6055a2c2a071005cf0c2954
inuEbisu <inuebisu@outlook.com>
2025-08-05 20:24:01 +0800

    refactor: improve library export structure

--- lib/fido2.dart
+++ lib/fido2.dart
@@ -1,9 +1,5 @@
 /// Library to parse FIDO2 request / response and interactive with FIDO2 authenticators.
 library fido2;
 
-export 'src/ctap.dart';
-export 'src/cose.dart';
-
-export 'src/ctap2/base.dart';
-export 'src/ctap2/pin.dart';
-export 'src/ctap2/credmgmt.dart';
+export 'fido2_client.dart';
+export 'fido2_server.dart';
---
+++ lib/fido2_server.dart
@@ -0,0 +1,8 @@
+/// Public API for the FIDO2 server (WebAuthn).
+library fido2_server;
+
+export 'src/server/base.dart';
+export 'src/server/config.dart';
+export 'src/server/entities/authenticator_data.dart';
+export 'src/server/entities/registration_data.dart';
+export 'src/server/entities/session_data.dart';
