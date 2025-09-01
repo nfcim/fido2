af06ecb7dae9224428c3347c95486baabac89592
inuEbisu <inuebisu@outlook.com>
2025-09-01 14:09:12 +0800

    fix: add verification data entity export to FIDO2 server

--- lib/fido2_server.dart
+++ lib/fido2_server.dart
@@ -5,3 +5,4 @@ export 'src/server/base.dart';
 export 'src/server/config.dart';
 export 'src/server/entities/authenticator_data.dart';
 export 'src/server/entities/registration_data.dart';
+export 'src/server/entities/verification_data.dart';
